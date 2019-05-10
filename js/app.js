5b19b6e1827429c731fbda212d4a49259af0022f
Eder Landaluce <ederlandaluze@gmail.com>
2019-05-10 08:27:49 +0200

    mostrar productos y categorias

---
+++ js/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+
+        
+    var htmlCode = "";
+
+    htmlCode += '';
+    htmlCode += '<img src="ButtCapper_Banner2.gif">';
+    htmlCode += '<button class="btn">Carrito</button>';
+
+    $('#cabecera').html(htmlCode);
+
+
+
+    $.getJSON('http://localhost:8080/proyectoV1/api/productos', function(productos){
+        console.log(productos);
+       
+
+        var htmlCode = "";
+
+        htmlCode += '';
+        
+
+        for (let index = 0; index < productos.length; index++) {
+            const producto = productos[index];
+            console.log(producto.nombre);
+            htmlCode += '<div class="p-2 w-25"  style="border:solid 2px black" align="center">';
+            htmlCode += '<img src="'+producto.imagen+'" style="max-width:100px">';
+            htmlCode += '<div id="info">';
+            htmlCode += '<p> '+producto.nombre+'</p>';
+            htmlCode += '<p> '+producto.precio+'€</p>';
+            htmlCode += '<button type="button" class="btn btn-dark">añadir a la cesta</button>';
+            htmlCode += '</div>';
+            htmlCode += '</div>';
+
+            
+        }
+     $('#productos').html(htmlCode);
+
+        
+    });
+    $.getJSON('http://localhost:8080/proyectoV1/api/categorias', function(categorias){
+        console.log(categorias);
+       
+
+        var htmlCode = "";
+
+        htmlCode += '';
+        
+
+        for (let index = 0; index < categorias.length; index++) {
+            const categoria = categorias[index];
+            console.log(categoria.nombre);
+            htmlCode += '<div id="categoria' +categoria.id_categoria+ '" data-idCategoria="'+categoria.id_categoria+'" class="categoria" style="border:solid 2px black" >';
+            htmlCode += '<p> '+categoria.nombre+'</p>';
+            htmlCode += '</div>';
+
+            
+        }
+     $('#categorias').html(htmlCode);
+
+        //anadir evento a categoria
+
+        
+
+    });
+});
