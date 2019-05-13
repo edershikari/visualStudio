7bf9ef9bc445b63fa20db58258ad197a6d7fbd80
Eukene Padilla <tukeplayer@gmail.com>
2019-05-13 14:41:15 +0200

    cambios superficiales en app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+        
+    $('#producto-card').hide();
         
     var htmlCode = "";
 
@@ -12,7 +13,59 @@ $(document).ready(function(){
     $.getJSON('http://localhost:8080/proyectoV1/api/productos', function(productos){
         console.log(productos);
         mostrarTodos(productos);
+        mostrarProducto(productos);
     });
+    function mostrarProducto(productos){
+                    //cuando se click una img se esconda todo y solo aparezcan los datos del producto
+                    $('#productos img').on("click", function(){
+                    
+                        //Cojer el id
+                        var id = $(this).data('id_producto'); //data: 
+        
+                        //Buscar el producto en el array
+                        var productoSeleccionado = buscarProducto(id);
+        
+                        //rellenar el card 
+                         rellenarElCard(productoSeleccionado);
+                         $('#cabecera').hide();
+                         $('#categorias').hide();
+                         $('#productos').children().hide();
+                         $('#producto-card').show();
+        
+                    });
+        
+                    function buscarProducto(id){
+                        for (let index = 0; index < productos.length; index++) {
+                            const producto = productos[index];
+                            
+                            if (id == producto.id_producto) {
+                                return producto;
+                            }
+                        }
+                    }
+                    var htmlCode="";
+                    htmlCode += '';
+
+                    function rellenarElCard(productoSeleccionado){
+        
+                        htmlCode += '<div class="card" style="width: 20rem;">';
+                        htmlCode += '<img src="'+productoSeleccionado.imagen+'" class="card-img-top" style="width:200px">';
+                        htmlCode += '<div class="card-body">';
+                        htmlCode += '<h5 class="card-title">'+productoSeleccionado.nombre+'</h5>';
+                        htmlCode += '<p class="card-text">'+productoSeleccionado.descripcion+'</p>';
+                        htmlCode += '<a href="index.html" class="btn btn-primary">Atrás</a>';
+                        htmlCode += '</div>';
+                        htmlCode += '</div>';
+                        
+                        // $('#producto-card h3').html(productoSeleccionado.nombre);
+                        // $('#descripcion').html(productoSeleccionado.descripcion);
+                        // $('#producto-card img').attr('src',productoSeleccionado.imagen);
+                        // $('#producto-card img').attr('style','width:200px;');
+                        // $('#precio').html(productoSeleccionado.precio+"€");
+                        $('#producto-card').html(htmlCode);
+
+                    }
+    }
 
     function mostrarTodos(productos){
         var htmlCode = "";
@@ -24,24 +77,26 @@ $(document).ready(function(){
             const producto = productos[index];
             //console.log(producto.nombre);
             htmlCode += '<div class="p-2 w-25"  style="border:solid 2px black" align="center">';
-            htmlCode += '<img src="'+producto.imagen+'" style="max-width:100px">';
+            htmlCode += '<img src="'+producto.imagen+'"  data-id_producto="'+producto.id_producto+'" style="max-width:100px">';
             htmlCode += '<div id="info">';
             htmlCode += '<p> '+producto.nombre+'</p>';
             htmlCode += '<p> '+producto.precio+'€</p>';
             htmlCode += '<button type="button" class="btn btn-dark">añadir  al carrito</button>';
             htmlCode += '</div>';
             htmlCode += '</div>';
-
-            
         }
         $('#productos').html(htmlCode); //Rellena todos los productos
+
         $( ".todosProductos" ).click(function(productos) { //boton con evento para mostrar todos los productos
             $.getJSON('http://localhost:8080/proyectoV1/api/productos', function(productos){
                 console.log(productos);
                 mostrarTodos(productos);
-            });     
+            }); 
+           
         });
+
     }
+    
         
 
     $.getJSON('http://localhost:8080/proyectoV1/api/categorias', function(categorias){ //lamada Json para rellenar categorias
@@ -83,7 +138,7 @@ $(document).ready(function(){
         });
 
 
-    });
+    });//api/categorias
 
    
 
